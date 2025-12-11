5eefe3561940fe7fed7bbb5dba81add5a88be4ec
Aser <aser@MacBook-Air-Aser.local>
2025-12-11 05:04:29 +0200

    THER-15: Registration API Implementation

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -48,7 +48,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',  # overriden per-view
+        'rest_framework.permissions.IsAuthenticated',
     ),
 }
 
