21a38afd7a6e66aad1fe8bbba03f318763a7216f
Sohila105 <sohila710@gmail.com>
2025-12-20 09:05:09 +0200

    THER-31 crud api done (#18)

--- backend/patients/views.py
+++ backend/patients/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# backend/patients/views.py
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Patient
+from .serializers import PatientSerializer
+from .permissions import IsTherapist, IsOwnerTherapist
+
+
+class PatientViewSet(viewsets.ModelViewSet):
+    serializer_class = PatientSerializer
+    permission_classes = [IsAuthenticated, IsTherapist, IsOwnerTherapist]
+
+    def get_queryset(self):
+        return Patient.objects.select_related("therapist").filter(therapist=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(therapist=self.request.user)
